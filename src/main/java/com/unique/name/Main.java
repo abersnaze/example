b64ab4a2c03ff98cb521fee0b2cf1f5f65172019
George Campbell <gscampbell@netflix.com>
2021-10-02 08:49:35 -0700

    edit: src/main/java/com/unique/name/Main.java to use the dependency

--- src/main/java/com/unique/name/Main.java
+++ src/main/java/com/unique/name/Main.java
@@ -1,7 +1,12 @@
 package com.unique.name;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(String[] args) throws JsonProcessingException {
+        JsonNode foo = new ObjectMapper().readTree("{ \"key\": [ 1, true, \"foo\" ] }");
+        System.out.println(foo);
     }
 }
